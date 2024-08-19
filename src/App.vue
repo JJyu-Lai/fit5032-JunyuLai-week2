f7c501ffccb42a676ce43457a368bfb9560da6ca
JJyu-Lai <jlai0058@student.monash.edu>
2024-08-19 14:28:10 +1000

    week 5 updata1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 // import JSON from './components/JSON.vue';
 import Form from './components/LoginForm.vue';
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue';
 </script>
 
 <template>
   <!-- <JSON /> -->
-  <Form />
+  <!-- <Form /> -->
+   <LibraryRegistrationForm />
 </template>
